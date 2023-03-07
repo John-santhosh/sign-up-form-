b48bf1c276e48bf2fb47558e95ef5c8a8997b493
john <johnsantosh2@gmail.com>
2023-03-07 21:51:01 +0530

    UI fixed and some js refactor

--- script/index.js
+++ script/index.js
@@ -3,36 +3,34 @@ let confirmPassWord = document.querySelector("#Cpword");
 let button = document.querySelector('#btn')
 let a = document.querySelectorAll(".form-content")[2].firstElementChild;
 
+
+// passwd mismatch 
+function misMatch(){
+    a.classList.add("error");
+    passWord.classList.add("invalid-border");
+    confirmPassWord.classList.add("invalid-border");
+    button.setAttribute("disabled", "");
+}
+
+// passwd match 
+function match(){
+    a.classList.remove("error");
+    passWord.classList.remove("invalid-border");
+    confirmPassWord.classList.remove("invalid-border");
+    button.removeAttribute("disabled");
+}
+
+
+
 confirmPassWord.addEventListener('keyup',function(){
     if(passWord.value==="" || confirmPassWord ==="") return 
-    if(passWord.value===confirmPassWord.value) {
-        console.log(1)
-        a.classList.remove("error");
-        passWord.classList.remove("invalid-border");
-        confirmPassWord.classList.remove("invalid-border");
-        button.removeAttribute("disabled")
-    }else{
-        a.classList.add("error");
-        passWord.classList.add("invalid-border");
-        confirmPassWord.classList.add("invalid-border");
-        button.setAttribute("disabled","");
-
-    }
+    passWord.value === confirmPassWord.value ? match() : misMatch() 
 })
+
+
 passWord.addEventListener('keyup',function(){
-    if (passWord.value === "" || confirmPassWord === "") return; 
-    if(passWord.value===confirmPassWord.value) {
-        console.log(1)
-        a.classList.remove("error");
-        passWord.classList.remove("invalid-border");
-        confirmPassWord.classList.remove("invalid-border");
-        button.removeAttribute("disabled");
-    }else{
-        a.classList.add("error");
-        passWord.classList.add("invalid-border");
-        confirmPassWord.classList.add("invalid-border");
-        button.setAttribute("disabled","");
-    }
+    if (passWord.value === "" || confirmPassWord === "") return;
+    passWord.value === confirmPassWord.value ? match() : misMatch(); 
 })
 
 
