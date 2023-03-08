6aaeee0c0b27da59ac34e37e254f724e46004e42
john <johnsantosh2@gmail.com>
2023-03-08 11:54:11 +0530

    new UI and code refactoring done

--- script/index.js
+++ script/index.js
@@ -1,12 +1,33 @@
 let passWord = document.querySelector("#pword");
 let confirmPassWord = document.querySelector("#Cpword");
 let button = document.querySelector('#btn')
-let a = document.querySelectorAll(".form-content")[2].firstElementChild;
+let errorMsg = document.querySelectorAll(".form-content")[2].firstElementChild;
+let successMsg = document.querySelector('.success')
+let passwordField = document.querySelectorAll('[type="password"]');
 
+// SuccessMsg
+function loginSuccess(){
+    successMsg.style.display="flex"
+    setTimeout(()=>{
+            successMsg.style.display = "none";
+    },3000)
+}
+
+// event listener's
+// create Account button 
+button.addEventListener("click", loginSuccess);
+
+// checking passwords 
+passwordField.forEach(each=>{
+    each.addEventListener('keyup',()=>{
+        if (passWord.value === "" || confirmPassWord === "") return;
+        passWord.value === confirmPassWord.value ? match() : misMatch(); 
+    })
+})
 
 // passwd mismatch 
 function misMatch(){
-    a.classList.add("error");
+    errorMsg.classList.add("error");
     passWord.classList.add("invalid-border");
     confirmPassWord.classList.add("invalid-border");
     button.setAttribute("disabled", "");
@@ -14,23 +35,12 @@ function misMatch(){
 
 // passwd match 
 function match(){
-    a.classList.remove("error");
+    errorMsg.classList.remove("error");
     passWord.classList.remove("invalid-border");
     confirmPassWord.classList.remove("invalid-border");
     button.removeAttribute("disabled");
 }
 
 
-
-confirmPassWord.addEventListener('keyup',function(){
-    if(passWord.value==="" || confirmPassWord ==="") return 
-    passWord.value === confirmPassWord.value ? match() : misMatch() 
-})
-
-
-passWord.addEventListener('keyup',function(){
-    if (passWord.value === "" || confirmPassWord === "") return;
-    passWord.value === confirmPassWord.value ? match() : misMatch(); 
-})
 
 
