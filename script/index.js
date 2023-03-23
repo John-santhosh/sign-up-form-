023499f0b72d5db69ad3556605f9017767685afd
john <johnsantosh2@gmail.com>
2023-03-23 16:17:46 +0530

    javaScript validation updated

--- script/index.js
+++ script/index.js
@@ -1,46 +1,47 @@
 let passWord = document.querySelector("#pword");
 let confirmPassWord = document.querySelector("#Cpword");
-let button = document.querySelector('#btn')
-let errorMsg = document.querySelectorAll(".form-content")[2].firstElementChild;
-let successMsg = document.querySelector('.success')
+let errorMsg = document.querySelector(".error");
+let successMsg = document.querySelector(".success");
 let passwordField = document.querySelectorAll('[type="password"]');
 
-// SuccessMsg
-function loginSuccess(){
-    successMsg.style.display="flex"
-    setTimeout(()=>{
-            successMsg.style.display = "none";
-    },3000)
+const formEl = document.forms.loginForm;
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  loginSuccess();
+  let inputs = [...formEl.elements];
+  inputs.forEach((element) => {
+    element.value = "";
+  });
+});
+
+// checking passwords
+passwordField.forEach((each) => {
+  each.addEventListener("keyup", () => {
+    if (passWord.value === "" || confirmPassWord === "") return;
+    passWord.value === confirmPassWord.value ? match() : misMatch();
+  });
+});
+
+// passwd mismatch
+function misMatch() {
+  errorMsg.classList.add("error");
+  passWord.classList.add("invalid-border");
+  confirmPassWord.classList.add("invalid-border");
 }
 
-// event listener's
-// create Account button 
-button.addEventListener("click", loginSuccess);
-
-// checking passwords 
-passwordField.forEach(each=>{
-    each.addEventListener('keyup',()=>{
-        if (passWord.value === "" || confirmPassWord === "") return;
-        passWord.value === confirmPassWord.value ? match() : misMatch(); 
-    })
-})
-
-// passwd mismatch 
-function misMatch(){
-    errorMsg.classList.add("error");
-    passWord.classList.add("invalid-border");
-    confirmPassWord.classList.add("invalid-border");
-    button.setAttribute("disabled", "");
+// passwd match
+function match() {
+  errorMsg.classList.remove("error");
+  passWord.classList.remove("invalid-border");
+  confirmPassWord.classList.remove("invalid-border");
 }
 
-// passwd match 
-function match(){
-    errorMsg.classList.remove("error");
-    passWord.classList.remove("invalid-border");
-    confirmPassWord.classList.remove("invalid-border");
-    button.removeAttribute("disabled");
+function loginSuccess() {
+  successMsg.style.transform = "scale(1)";
+  setTimeout(() => {
+    successMsg.style.transform = "scale(0)";
+  }, 3000);
 }
 
-
-
-
+// console.log(formEl);
